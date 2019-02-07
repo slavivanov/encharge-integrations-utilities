ad7ed8af4be509377f77665e2367d3ccb407cd82
slav <murphybgbs@gmail.com>
2019-02-07 21:04:28 +0200

    Updated file paths

--- schema/resolve-internal-refs.js
+++ schema/resolve-internal-refs.js
@@ -1,24 +1,28 @@
 const SwaggerParser = require("swagger-parser");
 const fs = require("fs");
+const path = require("path");
 
-SwaggerParser.dereference(
-  require("../definitions/resolved-external.api.oas3.json"),
-  { circular: "ignore" },
-  (err, res) => {
-    if (err) {
-      console.log(err);
-      process.exitCode = 1;
-      return;
-    }
-    fs.writeFile(
-      "definitions/resolved.api.oas3.json",
-      JSON.stringify(res, null, 2),
-      er => {
-        if (er) {
-          console.log(er);
-          process.exitCode = 1;
-        }
-      }
-    );
+// Figure out which is the OAS definition file
+// const argv = require('minimist')(process.argv.slice(2));
+const filename =
+  process.argv[2] || "definitions/resolved-external.api.oas3.json";
+const filepath = path.resolve(process.cwd(), filename);
+let schema = require(filepath);
+
+SwaggerParser.dereference(schema, { circular: "ignore" }, (err, res) => {
+  if (err) {
+    console.log(err);
+    process.exitCode = 1;
+    return;
   }
-);
+  fs.writeFile(
+    path.resolve(process.cwd(), "definitions/resolved.api.oas3.json"),
+    JSON.stringify(res, null, 2),
+    er => {
+      if (er) {
+        console.log(er);
+        process.exitCode = 1;
+      }
+    }
+  );
+});
